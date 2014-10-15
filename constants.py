df9753970c8668172e46dfefb32977441d36a2cf
r-zemblys <raimondas.zemblys@gmail.com>
2014-10-15 13:15:15 +0200

    merged with shared files from hdf2wide util

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
-__author__ = 'RZ'
+__author__ = 'Sol'
+__version__ = 'RZ'
 import numpy as np
 
 et_nan_values = dict()
@@ -14,7 +15,96 @@ et_nan_values['t60xl'] = {'x': -1.0, 'y': -1.0}
 et_nan_values['tx300'] = {'x': -1.0, 'y': -1.0}
 et_nan_values['x2'] = {'x': -1.0, 'y': -1.0}
 et_nan_values['dpi'] = {'x': -10000, 'y': -10000}
-   
+
+wide_row_dtype = np.dtype([
+    ('subject_id', np.uint8),
+    ('display_refresh_rate', np.float32),
+    ('eyetracker_model', str, 32),
+    ('dot_deg_sz', np.float32),
+    ('eyetracker_sampling_rate', np.float32),
+    ('eyetracker_mode', str, 16),
+    ('fix_stim_center_size_pix', np.uint8),
+    ('operator', str, 8),
+    ('eyetracker_id', np.uint8),
+    ('display_width_pix', np.float32),
+    ('display_height_pix', np.float32),
+    ('screen_width', np.float32),
+    ('screen_height', np.float32),
+    ('eye_distance', np.float32),
+    ('SESSION_ID', np.uint8),
+    ('trial_id', np.uint16),
+    ('TRIAL_START', np.float32),
+    ('TRIAL_END', np.float32),
+    ('posx', np.float32),
+    ('posy', np.float32),
+    ('dt', np.float32),
+    ('ROW_INDEX', np.uint8),
+    ('BLOCK', str, 6),
+    ('session_id', np.uint8),
+    ('device_time', np.float32),
+    ('time', np.float32),
+    ('left_gaze_x', np.float32),
+    ('left_gaze_y', np.float32),
+    ('left_pupil_measure1', np.float32),
+    ('right_gaze_x', np.float32),
+    ('right_gaze_y', np.float32),
+    ('right_pupil_measure1', np.float32),
+    ('status', np.uint8),
+    ('target_angle_x', np.float32),
+    ('target_angle_y', np.float32),
+    ('left_angle_x', np.float32),
+    ('left_angle_y', np.float32),
+    ('right_angle_x', np.float32),
+    ('right_angle_y', np.float32)
+    ])
+    
+
+
+from collections import OrderedDict
+msg_txt_mappings = OrderedDict()
+msg_txt_mappings['Participant ID'] = 'subject_id'
+msg_txt_mappings['Monitor refresh rate (Hz)'] = 'display_refresh_rate'
+msg_txt_mappings['Eye tracker'] = 'eyetracker_model'
+msg_txt_mappings['dotStimSize (deg)'] = 'dot_deg_sz'
+msg_txt_mappings['Tracker SamplingRate'] = 'eyetracker_sampling_rate'
+msg_txt_mappings['Tracker mode'] = 'eyetracker_mode'
+msg_txt_mappings['dotStimCenter (px)'] = 'fix_stim_center_size_pix'
+msg_txt_mappings['Operator'] = 'operator'
+msg_txt_mappings['Tracker ID'] = 'eyetracker_id'
+msg_txt_mappings['Stimulus Screen ID'] = 'display_width_pix'
+msg_txt_mappings['Stimulus Screen ID2'] = 'display_height_pix'
+msg_txt_mapping_keys = msg_txt_mappings.keys()
+msg_txt_mapping_values = msg_txt_mappings.values()
+
+#Fixes ROW_INDEX in DPI calibration routine for 2014 Apr-May EDQ recordings
+dpi_cal_fix = dict([
+    (0,21),
+    (1,7),
+    (2,22),
+    (3,14),
+    (4,20),
+    (5,10),
+    (6,17),
+    (7,6),
+    (8,15),
+    (9,0),
+    (10,18),
+    (11,11),
+    (12,4),
+    (13,5),
+    (14,13),
+    (15,3),
+    (16,12),
+    (17,19),
+    (18,24),
+    (19,8),
+    (20,2),
+    (21,16),
+    (22,1),
+    (23,23),
+    (24,9),
+    ])
+
 stim_dtype = np.dtype([
 
     ('eyetracker_model', str, 32),
@@ -110,4 +200,48 @@ stim_pos_mappings=dict([
     ('angle','target_angle_'),
     ('gaze', 'pos'),
     ])
-    
+# iohub EventConstants values, as of June 13th, 2014.
+# Copied so that iohub does not need to be a dependency of conversion script
+KEYBOARD_INPUT = 20
+KEYBOARD_KEY = 21
+KEYBOARD_PRESS = 22
+KEYBOARD_RELEASE = 23
+
+MOUSE_INPUT = 30
+MOUSE_BUTTON = 31
+MOUSE_BUTTON_PRESS = 32
+MOUSE_BUTTON_RELEASE = 33
+MOUSE_DOUBLE_CLICK = 34
+MOUSE_SCROLL = 35
+MOUSE_MOVE = 36
+MOUSE_DRAG = 37
+
+TOUCH = 40
+TOUCH_MOVE = 41
+TOUCH_PRESS = 42
+TOUCH_RELEASE = 43
+
+EYETRACKER = 50
+MONOCULAR_EYE_SAMPLE = 51
+BINOCULAR_EYE_SAMPLE = 52
+FIXATION_START = 53
+FIXATION_END = 54
+SACCADE_START = 55
+SACCADE_END = 56
+BLINK_START = 57
+BLINK_END = 58
+
+GAMEPAD_STATE_CHANGE = 81
+GAMEPAD_DISCONNECT = 82
+
+DIGITAL_INPUT = 101
+ANALOG_INPUT = 102
+THRESHOLD = 103
+
+SERIAL_INPUT = 105
+SERIAL_BYTE_CHANGE = 106
+
+MULTI_CHANNEL_ANALOG_INPUT = 122
+
+MESSAGE = 151
+LOG = 152
--- edq_shared.py
+++ edq_shared.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  7 14:38:28 2014
-
-@author: huml-rzm
-"""
-
 import os
+
 import numpy as np
+
+from constants import *
 
 def nabs(file_path):
     """
@@ -56,12 +53,12 @@ def parseTrackerMode(eyetracker_mode):
         return ['left', 'right']
     else:
         return [eyetracker_mode.split(' ')[0].lower()]
-        
+
 def rolling_window(a, window):
     shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
     strides = a.strides + (a.strides[-1],)
-    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
-    
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)        
+
 def add_field(a, descr):
     """Return a new array that is like "a", but has additional fields.
 
@@ -92,3 +89,264 @@ def add_field(a, descr):
     for name in a.dtype.names:
         b[name] = a[name]
     return b
+    
+    
+### VisualAngleCalc
+"""
+Copied code from iohub
+(trying to keep dependencies low for this conversion script)
+
+__author__ = 'Sol'
+"""
+arctan = np.arctan2
+rad2deg = np.rad2deg
+hypot = np.hypot
+np_abs = np.abs
+np_sqrt = np.sqrt
+
+class VisualAngleCalc(object):
+    def __init__(self, display_size_mm, display_res_pix, eye_distance_mm=None):
+        """
+        Used to store calibrated surface information and eye to screen distance
+        so that pixel positions can be converted to visual degree positions.
+
+        Note: The information for display_size_mm,display_res_pix, and default
+        eye_distance_mm could all be read automatically when opening a ioDataStore
+        file. This automation should be implemented in a future release.
+        """
+        self._display_width = display_size_mm[0]
+        self._display_height = display_size_mm[1]
+        self._display_x_resolution = display_res_pix[0]
+        self._display_y_resolution = display_res_pix[1]
+        self._eye_distance_mm = eye_distance_mm
+        self.mmpp_x = self._display_width / self._display_x_resolution
+        self.mmpp_y = self._display_height / self._display_y_resolution
+
+    def pix2deg(self, pixel_x, pixel_y=None, eye_distance_mm=None):
+        """
+        Stimulus positions (pixel_x,pixel_y) are defined in x and y pixel units,
+        with the origin (0,0) being at the **center** of the display, as to match
+        the PsychoPy pix unit coord type.
+
+        The pix2deg method is vectorized, meaning that is will perform the
+        pixel to angle calculations on all elements of the provided pixel
+        position numpy arrays in one numpy call.
+
+        The conversion process can use either a fixed eye to calibration
+        plane distance, or a numpy array of eye distances passed as
+        eye_distance_mm. In this case the eye distance array must be the same
+        length as pixel_x, pixel_y arrays.
+        """
+        eye_dist_mm = self._eye_distance_mm
+        if eye_distance_mm is not None:
+            eye_dist_mm = eye_distance_mm
+
+        x_mm = self.mmpp_x * pixel_x
+        y_mm = self.mmpp_y * pixel_y
+
+        Ah = arctan(x_mm, hypot(eye_dist_mm, y_mm))
+        Av = arctan(y_mm, hypot(eye_dist_mm, x_mm))
+
+        return rad2deg(Ah), rad2deg(Av)
+###
+        
+        
+def initStim(data):
+    """
+    Creates /one-row-per-target/ data matrix 
+    
+    @author: Raimondas Zemblys
+    @email: raimondas.zemblys@humlab.lu.se
+    """
+    stim_change_ind = np.where(np.hstack((1,np.diff(data['trial_id']))) == 1)
+    stim_change_count = len(stim_change_ind[0])
+    
+    wsa_list=np.empty_like(data['eyetracker_model'][stim_change_ind])
+    wsa_list[:]='nan'
+    
+    stim = np.array(
+               zip(
+                   data['eyetracker_model'][stim_change_ind],
+                   data['eyetracker_sampling_rate'][stim_change_ind],                 
+                   data['eyetracker_mode'][stim_change_ind],
+                   np.ones(stim_change_count) * getGeometry(data[0]), 
+                   data['operator'][stim_change_ind],
+
+                   data['subject_id'][stim_change_ind],
+                   data['trial_id'][stim_change_ind], 
+                   data['ROW_INDEX'][stim_change_ind], 
+                   data['dt'][stim_change_ind], 
+                   data['TRIAL_START'][stim_change_ind],
+                   data['TRIAL_END'][stim_change_ind],
+                   data['posx'][stim_change_ind],
+                   data['posy'][stim_change_ind],
+                   data['target_angle_x'][stim_change_ind],
+                   data['target_angle_y'][stim_change_ind],
+                   
+                   wsa_list.tolist(),
+                   *np.zeros((65, stim_change_count)) * np.nan
+               ), dtype=stim_dtype
+           )
+    
+    #Remove first and last stimuli
+    return stim[1:-1]
+    
+    
+def detect_rollingWin(data, **args):
+    """
+    Fills /one-row-per-target/ data matrix
+    
+    @author: Raimondas Zemblys
+    @email: raimondas.zemblys@humlab.lu.se
+    """    
+    win_size = args['win_size']
+    win_size_sample = np.int16(win_size*data['eyetracker_sampling_rate'][0])
+    window_skip = args['window_skip']
+    selection_algorithms = args['wsa']    
+    
+    measures=dict()
+    stim_full=[]
+    for eye in parseTrackerMode(data['eyetracker_mode'][0]):   
+            
+        measures.update(rolling_measures(data, eye, 'gaze', win_size_sample))
+        measures.update(rolling_measures(data, eye, 'angle', win_size_sample))
+        
+        for wsa in selection_algorithms:
+            stim = initStim(data)
+    
+            for stim_ind, stim_row in enumerate(stim):
+                
+                analysis_range = (data['time'] >= stim_row['TRIAL_START']+window_skip) \
+                               & (data['time'] <= stim_row['TRIAL_START']+1-win_size)
+                
+                analysis_range_full= (data['time'] >= stim_row['TRIAL_START']) \
+                                   & (data['time'] <= stim_row['TRIAL_START']+1)
+                
+                #needs to be a number to identify starting sample of a window               
+                analysis_range=np.squeeze(np.argwhere(analysis_range==True))
+                analysis_range_full=np.squeeze(np.argwhere(analysis_range_full==True))
+                                  
+                measures_rangeRMS = measures['_'.join((eye, 'angle', 'RMS'))][analysis_range]
+                measures_rangeACC = measures['_'.join((eye, 'angle', 'ACC'))][analysis_range]
+                measures_rangeSTD = measures['_'.join((eye, 'angle', 'STD'))][analysis_range]
+                
+                if wsa == 'fiona':
+                    measures_range = measures_rangeRMS #Fiona RMS 
+                elif wsa == 'dixon1':
+                    measures_range = measures_rangeACC*measures_rangeSTD #Dixons's measure No. 1
+                elif wsa == 'dixon2':
+                    measures_range = measures_rangeACC+measures_rangeSTD #Dixons's measure No. 2
+                elif wsa == 'dixon3':
+                    measures_range = measures_rangeACC**2+measures_rangeSTD**2 #Dixons's measure No. 3
+                elif wsa == 'jeff':
+                    #Jeff's measure                        
+                    std_thres = np.nanmin(measures_rangeSTD)*5
+                    measures_rangeACC[measures_rangeSTD>std_thres] = np.nan
+                    measures_range = measures_rangeACC
+                
+    
+                if np.sum(np.isfinite(measures_range)) > 0: #handle all-nan slice
+                    IND = analysis_range[np.nanargmin(measures_range)]
+                    
+                    #save measures to stim
+                    stim['_'.join((eye, 'gaze', 'ind'))][stim_ind] = IND
+                    stim['_'.join((eye, 'angle', 'ind'))][stim_ind] = IND
+                    
+                    stim['_'.join((eye, 'window_onset'))][stim_ind] = data['time'][IND]-stim_row['TRIAL_START']
+                    stim['_'.join((eye, 'sample_count'))][stim_ind] = win_size_sample
+                    stim['_'.join((eye, 'actual_win_size'))][stim_ind] = data['time'][IND+win_size_sample]-data['time'][IND]
+                    
+                    stim['invalid_sample_count'][stim_ind] = np.sum(np.isnan(data[eye+'_gaze_x'][analysis_range_full]) |
+                                                                    np.isnan(data[eye+'_gaze_y'][analysis_range_full])
+                                                             )
+                    
+                    stim['wsa'][:] = wsa
+                    stim['win_size'][:] =  win_size
+                    stim['window_skip'][:] =  window_skip
+                    
+                    for key in measures.keys():
+                        stim[key][stim_ind]=measures[key][IND]  
+            
+            stim_full.extend(stim.tolist())
+                  
+    return np.array(stim_full, dtype=stim_dtype)
+    
+    
+def rolling_measures(data, eye, units, win_size):
+    """
+    Calculates rolling window measures 
+    
+    @author: Raimondas Zemblys
+    @email: raimondas.zemblys@humlab.lu.se
+    """    
+    #inter-sample distances
+    isd = np.diff(zip(data['_'.join((eye, units, 'x'))], 
+                      data['_'.join((eye, units, 'y'))]), axis=0)
+    isd = np.vstack((isd[:,0], isd[:,1], np.hypot(isd[:,0], isd[:,1]))).T
+    
+
+    measures=dict()
+    
+    #RMS
+    measures['_'.join((eye, units, 'RMS', 'x'))] = np.sqrt(np.mean(rolling_window(isd[:,0], win_size-1)**2, 1))
+    measures['_'.join((eye, units, 'RMS', 'y'))] = np.sqrt(np.mean(rolling_window(isd[:,1], win_size-1)**2, 1))
+    measures['_'.join((eye, units, 'RMS'))] = np.sqrt(np.mean(rolling_window(isd[:,2], win_size-1)**2, 1))
+    
+    rolling_data_x = rolling_window(data['_'.join((eye, units, 'x'))], win_size)
+    rolling_data_y = rolling_window(data['_'.join((eye, units, 'y'))], win_size)
+    
+    #STD  
+    measures['_'.join((eye, units, 'STD', 'x'))] = np.std(rolling_data_x, axis=1)
+    measures['_'.join((eye, units, 'STD', 'y'))] = np.std(rolling_data_y, axis=1)
+    measures['_'.join((eye, units, 'STD'))] = np.hypot(measures['_'.join((eye, units, 'STD', 'x'))],
+                                                       measures['_'.join((eye, units, 'STD', 'y'))]   
+                                              )
+    
+    #ACC
+    fix = [np.median(rolling_data_x, axis=1),
+           np.median(rolling_data_y, axis=1)]
+
+    measures['_'.join((eye, units, 'ACC', 'x'))] = fix[0] - data[stim_pos_mappings[units]+'x'][:-win_size+1]
+    measures['_'.join((eye, units, 'ACC', 'y'))] = fix[1] - data[stim_pos_mappings[units]+'y'][:-win_size+1]
+    measures['_'.join((eye, units, 'ACC'))] = np.hypot(measures['_'.join((eye, units, 'ACC', 'x'))],
+                                                       measures['_'.join((eye, units, 'ACC', 'y'))]   
+                                              )
+    
+    measures['_'.join((eye, units, 'fix', 'x'))] = fix[0]
+    measures['_'.join((eye, units, 'fix', 'y'))] = fix[1]
+    
+    measures['_'.join((eye, units, 'range', 'x'))] = np.nanmax(rolling_data_x, axis=1)-np.nanmin(rolling_data_x, axis=1)
+    measures['_'.join((eye, units, 'range', 'y'))] = np.nanmax(rolling_data_y, axis=1)-np.nanmin(rolling_data_y, axis=1)
+    
+    return measures
+    
+def getGeometry(data):
+    """
+    Calculates pix2deg values, based on simple geometry 
+    
+    @author: Raimondas Zemblys
+    @email: raimondas.zemblys@humlab.lu.se
+    """    
+    return np.mean((1/(np.degrees(2*np.arctan(data['screen_width']/(2*data['eye_distance'])))/data['display_width_pix']),
+                    1/(np.degrees(2*np.arctan(data['screen_height']/(2*data['eye_distance'])))/data['display_height_pix'])))
+                    
+def filter_trackloss(data, et_model=None, fill=np.nan):
+    """
+    Trackloss filter. Replaces invalid samples with /fill/
+    
+    @author: Raimondas Zemblys
+    @email: raimondas.zemblys@humlab.lu.se
+    """   
+    #TODO: Filter off-screen, off-pshysical limit samples
+    for eye in ['left', 'right']:
+        trackloss = (data['_'.join((eye, 'gaze_x'))] == et_nan_values[et_model]['x']) | \
+                    (data['_'.join((eye, 'gaze_y'))] == et_nan_values[et_model]['y'])
+        if et_model == 'dpi':
+            trackloss = np.bitwise_or(trackloss, data['status'] < 4.0)
+            
+        data['_'.join((eye, 'gaze_x'))][trackloss] = fill
+        data['_'.join((eye, 'gaze_y'))][trackloss] = fill
+        data['_'.join((eye, 'angle_x'))][trackloss] = fill
+        data['_'.join((eye, 'angle_y'))][trackloss] = fill
+        
+    return data
